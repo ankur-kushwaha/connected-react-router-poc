981a4d39b884bd878b8ea90409f5435aa10343df
Ankur Kushwaha <ankur.kushwaha@birdeye.com>
2017-07-27 23:14:38 +0530

    setup done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,29 @@
-import React from "react";
-import { sum } from "./util";
 
-const Home = () =>
-  <div>
-    Home {sum()}
-  </div>;
+import { sum } from "./util";
+import { Toggle } from '@birdeye/core'
+
+
+import React, { Component } from 'react';
+
+class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      flag: false
+    }
+  }
+  render() {
+    return (
+      <div>
+        Home
+        <Toggle checked={this.state.flag} onChange={(e) => {
+          this.setState({
+            flag: e.target.checked
+          })
+        }} />
+      </div>
+    );
+  }
+}
 
 export default Home;
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require("webpack")
 module.exports = {
   devtool: "cheap-module-source-map",
   entry: [
+    "babel-polyfill",
     "react-hot-loader/patch",
     "webpack-hot-middleware/client",
     path.resolve("src/index.js")
@@ -17,10 +18,37 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?/,
+        exclude: /.*node_modules((?!@birdeye).)*$/,
         // Don't use .babelrc in `yarn link`-ed dependency's directory and use in current direction instead
         loader:
-          "babel-loader?babelrc=false&extends=" +
-          path.resolve(__dirname, ".babelrc")
+        "babel-loader?babelrc=false&extends=" +
+        path.resolve(__dirname, ".babelrc")
+      }, {
+        test: /\.css$/,
+        loaders: ["style-loader", "css-loader"],
+        include: path.resolve(__dirname, "../")
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 8192,
+            name: "fonts/[name].[ext]?[hash]"
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+              name: "images/[name].[ext]?[hash]"
+            }
+          }
+        ]
       }
     ]
   },
